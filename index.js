d0a91a16cdbb8744c49662c1babcd453b937de08
Owethu927 <owethusilela@gmail.com>
2024-09-13 14:11:22 +0100

    Added a different image for creativity and remove another one changed the imaged sourced in the html added firebse in the javascript

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+// Importing the firebase links//
+
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+const appSettings = {
+    databaseURL: "https://mobile-app-7f6c8-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    push (shoppingListInDB, inputValue) //
     
     console.log(inputValue)
 })
