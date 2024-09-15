ffe0e63c2a84478b8a52323b65c6d31e0605b895
Owethu927 <owethusilela@gmail.com>
2024-09-15 13:47:10 +0100

    Made changes to the html and css

--- index.js
+++ index.js
@@ -14,10 +14,33 @@ const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    push (shoppingListInDB, inputValue) //
-    
-    console.log(inputValue)
+
+    push(shoppingListInDB, inputValue)
+
+    clearInputFieldEl()
 })
+
+// Creating a fuction to be able to update the items in realtime//
+onValue(shoppingListInDB, function(snapshot) {
+    let itemArray = Object.values(snapshot.val())
+
+    clearShoppingListEl()
+
+    for (let i = 0; i < itemsArray.length; i++) {
+        appendItemToShoppingListEl(itemArray[i])
+    }
+}) 
+// Creating a function to clear the input after putting a list//
+
+function clearInputFieldEL() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(inputValue) {
+    shoppingListEL.innerHTML += `<li>${inputValue}</li>`
+}
+
